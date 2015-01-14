5d9c044affa7b27e1b1c94be1cdc23d20d40b604
ligiatosa <ligia.tosa@yahoo.com>
2015-01-14 05:32:10 +0200

    -

---
+++ FricaDeGaze/Assets/AddSpider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddSpider : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject enemy;
+	public misca2 sc;
+	
+	IEnumerator Start() {
+		for (int i = 0; i < 10; i++) {
+			yield return new WaitForSeconds(Random.Range(0, 5.0f));
+			Instantiate(enemy);
+
+			enemy.transform.position = new Vector3(Random.Range(-2.5f, 2.5f),0,Random.Range(-0.5f, 0.5f));
+			sc=enemy.AddComponent("misca2") as misca2;
+
+			//sc.Invoke("Start()",0);
+
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
